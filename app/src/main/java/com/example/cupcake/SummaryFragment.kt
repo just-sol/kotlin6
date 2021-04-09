7d0600c156fb5f743e0cd321e60d5eed1eeb0289
just-sol <hsyshs5498@naver.com>
2021-04-09 22:17:55 +0900

    commit

---
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -0,0 +1,46 @@
+package com.example.cupcake
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.cupcake.databinding.FragmentSummaryBinding
+import com.example.cupcake.model.OrderViewModel
+
+class SummaryFragment : Fragment() {
+
+    private var binding: FragmentSummaryBinding? = null
+
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentSummaryBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
+            summaryFragment = this@SummaryFragment
+        }
+    }
+
+    fun sendOrder() {
+        Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
